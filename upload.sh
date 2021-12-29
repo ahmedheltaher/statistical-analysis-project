cb3d3063be119b2fd07230d322dc383da3699799
ahmedheltaher <ahmedheltaher@gmail.com>
2021-12-29 22:03:52 +0200

    init the app

---
+++ upload.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+border () {
+    local str="$*"      # Put all arguments into single string
+    local len=${#str}
+    local i
+    for (( i = 0; i < len + 4; ++i )); do
+        printf '-'
+    done
+    printf "\n| $str |\n"
+    for (( i = 0; i < len + 4; ++i )); do
+        printf '-'
+    done
+    echo
+}
+
+git add .
+
+read -p "Commit Message: " message
+git commit -m "$message"
+
+declare -a repositories=("origin" "heroku")
+
+for repo in "${repositories[@]}"
+do
+    git push "$repo" master
+    border "Pushed the Commit to "$repo" Sucessfuly"
+done
